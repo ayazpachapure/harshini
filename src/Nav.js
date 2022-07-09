564f98daf0e1f724d26e10dbfdda82b6114b78c8
ayazpachapure <108693166+ayazpachapure@users.noreply.github.com>
2022-07-09 22:13:57 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -22,21 +22,21 @@ function Nav(){
 
 
 
-      <Link to="/" style={{fontSize:"35px", lineHeight:"25px", fontFamily:"font-family: 'Cormorant Garamond', serif;",color:"white",textDecoration:"none",textAlign:"center",position:"absolute",bottom:"2px",marginLeft:"80px",bottom:"11px"}}>HARSHINI</Link>
+      <Link to="/" style={{fontSize:"35px", lineHeight:"25px", fontFamily:"font-family: 'Cormorant Garamond', serif;",color:"black",textDecoration:"none",textAlign:"center",position:"absolute",bottom:"2px",marginLeft:"80px",bottom:"11px"}}>HARSHINI</Link>
       </div>
 
       <div className="nav">
-      <Link to="/works" className="text " style={{padding:"0 22px 0 25px", borderRight:"1px solid", lineHeight:2,borderRightColor:"white"}}> Works </Link>
+      <Link to="/works" className="text " style={{padding:"0 22px 0 25px",  lineHeight:2,}}> Works </Link>
 
 
-      <Link to="/bio" className="text" style={{padding:"0 22px 0 25px", borderRight:"1px solid", lineHeight:2,borderRightColor: "white"}}> Bio </Link>
+      <Link to="/bio" className="text" style={{padding:"0 22px 0 25px", lineHeight:2,}}> Bio </Link>
 
 
-      <Link to="/question" className="text" style={{padding:"0 22px 0 25px", borderRight:"1px solid", lineHeight:2,borderRightColor: "white"}}> Questions </Link>
+      <Link to="/question" className="text" style={{padding:"0 22px 0 25px",  lineHeight:2,}}> Questions </Link>
 
 
       <Link to="/news" className="text"
-      style={{padding:"0 22px 0 25px", borderRight:"1px solid", lineHeight:2,borderRightColor:"white",marginRight: "17px"}}> NewsLetter </Link>
+      style={{padding:"0 22px 0 25px", lineHeight:2,marginRight: "17px"}}> NewsLetter </Link>
       <div style={{fontSize:"30px"}}>
       <a href="#" className="fa fa-facebook"></a>
       <a href="#" className="fa fa-twitter"></a>
