e3429cdc7c8aa1342ef2fc0ea3e9d38e4ab926be
ayazpachapure <108693166+ayazpachapure@users.noreply.github.com>
2022-07-09 22:04:21 +0530

    Add files via upload

---
+++ src/array.js
@@ -0,0 +1,27 @@
+import rose1 from "./rose1.jpg"
+import rose4 from "./rose4.jpg"
+import rose3 from "./rose3.png"
+
+
+
+
+export const images=[
+   {
+      title:"",
+      subtitle:"",
+      img:rose3,
+   },
+
+   {
+      title:"",
+      subtitle:"",
+      img:rose4,
+   },
+
+   {
+      title:"",
+      subtitle:"",
+      img:rose1,
+   },
+
+];
